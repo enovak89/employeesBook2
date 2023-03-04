ed49931c6523f39453a97cc1a1fe8f1b757b81a6
enovak89 <enovak89@mail.ru>
2023-03-04 23:31:10 +0300

    Init commit

---
+++ src/test/java/com/example/employeesbook2/EmployeesBook2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeesbook2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesBook2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
